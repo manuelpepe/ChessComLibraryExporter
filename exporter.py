57315a25ef834fbc9e50f1196ed43bb27a429df2
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2022-06-23 00:24:52 -0300

    games page crawling

--- exporter.py
+++ exporter.py
@@ -1,3 +1,5 @@
+import time
+
 from dataclasses import dataclass, field
 
 from selenium import webdriver
@@ -7,7 +9,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 
 
 @dataclass
@@ -24,8 +26,8 @@ class Collection:
     games: list[Game] = field(default_factory=list)
 
 
-def _safe_find(driver, *args, method: str = "find_elements"):
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located(args))
+def _safe_find(driver, *args, method: str = "find_elements", timeout: int = 20):
+    WebDriverWait(driver, timeout).until(EC.presence_of_element_located(args))
     return getattr(driver, method)(*args)
 
 
@@ -47,6 +49,31 @@ def find_game_pgn(driver: WebDriver, game_details_box: WebElement):
     close_share_modal = _safe_find(driver, By.CLASS_NAME, "ui_outside-close-component", method="find_element")
     close_share_modal.click()
     return pgn
+
+
+def load_games_from_page(driver: WebDriver, games: list[WebElement]) -> list[Game]:
+    game_objects = []
+    for game in games:
+        game.click()   # Expands _game_details_box
+        _game_details_box = game.parent.find_element(By.CLASS_NAME, "game-details-more")
+        link = _game_details_box.find_element(By.CSS_SELECTOR, "a.game-details-btn-component")
+        obj = Game(
+            title=find_game_title(game),
+            link=link.get_attribute("href"),
+            pgn=find_game_pgn(driver, _game_details_box)
+        )
+        game_objects.append(obj)
+        game.click()  # Closes _game_details_box
+    return game_objects
+
+
+def get_next_page_button(driver) -> None | WebElement:
+    try:
+        _next_page_button = driver.find_element(By.CSS_SELECTOR,'.ui_pagination-item-component[aria-label="Next Page"]')
+        if not _next_page_button.get_attribute("disabled"):
+            return _next_page_button
+    except NoSuchElementException:
+        return None
 
 
 class Scrapper:
@@ -83,18 +110,20 @@ class Scrapper:
         for collection in self.collections:
             print(f"Retrieving games from: '{collection.title}' ({collection.link})")
             self.driver.get(collection.link)
-            games = _safe_find(self.driver, By.CLASS_NAME, "game-item-component")
-            for game in games:
-                game.click()   # Expands _game_details_box
-                _game_details_box = game.parent.find_element(By.CLASS_NAME, "game-details-more")
-                link = _game_details_box.find_element(By.CSS_SELECTOR, "a.game-details-btn-component")
-                obj = Game(
-                    title=find_game_title(game),
-                    link=link.get_attribute("href"),
-                    pgn=find_game_pgn(self.driver, _game_details_box)
-                )
-                collection.games.append(obj)
-                game.click()  # Closes _game_details_box
+            self._populate_page_into_collection(collection)
+            next_page_button = get_next_page_button(self.driver)
+            while next_page_button:
+                next_page_button.click()
+                time.sleep(1)  # FIXME: Instead of sleeping an arbitrary ammount, some kind of check
+                               # should be performed on the UI. (maybe tracking the current page and checking
+                               # the specific page selector styling)
+                self._populate_page_into_collection(collection)
+                next_page_button = get_next_page_button(self.driver)
+                
+    def _populate_page_into_collection(self, collection):
+        games = _safe_find(self.driver, By.CLASS_NAME, "game-item-component")
+        game_objects = load_games_from_page(self.driver, games)
+        collection.games.extend(game_objects)
         
     def _end(self):
         self.driver.close()
