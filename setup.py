d4fdc516979b4b016ae8a578017c6173eaa91099
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2022-06-25 18:45:56 -0300

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(README, "r") as fp:
 
 setup(
     name="ChessComLibraryExport",
-    version="0.4.0",
+    version="0.5.0",
     description="Download your whole Chess.com Library (chess.com/library)",
     author="Manuel Pepe",
     author_email="manuelpepe-dev@outlook.com.ar",
