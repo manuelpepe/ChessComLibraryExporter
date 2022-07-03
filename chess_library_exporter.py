8c49c42ad0c4de8c133c9963787efabb7aebd197
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2022-07-03 20:25:13 -0300

    Bump version: 0.6.1 → 0.7.0

--- chess_library_exporter.py
+++ chess_library_exporter.py
@@ -21,7 +21,7 @@ from dotenv import load_dotenv
 from pathvalidate import sanitize_filename
 
 
-__version__ = "0.6.1"
+__version__ = "0.7.0"
 
 
 @dataclass
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(README, "r") as fp:
 
 setup(
     name="ChessComLibraryExporter",
-    version="0.6.1",
+    version="0.7.0",
     description="Download your whole Chess.com Library (chess.com/library)",
     author="Manuel Pepe",
     author_email="manuelpepe-dev@outlook.com.ar",
